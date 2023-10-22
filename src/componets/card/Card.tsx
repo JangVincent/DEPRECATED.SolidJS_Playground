46c0ab06bd79f32a10c69c685fdc5f3ff55a1f59
Phantola <phantoladev@gmail.com>
2023-10-22 13:07:35 +0900

    Add card compo for test tailwind

---
+++ src/componets/card/Card.tsx
@@ -0,0 +1,11 @@
+export function Card() {
+  return (
+    <>
+      <div class="grid place-items-center min-h-screen">
+        <div class="h-[160px] aspect aspect-[2] rounded-[16px] shadow-[0_0_0_4px_hsl(0_0%_0%_/_15%)]">
+          This is Card
+        </div>
+      </div>
+    </>
+  );
+}
