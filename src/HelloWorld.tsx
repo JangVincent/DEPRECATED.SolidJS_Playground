eafa25fb943a4ccd99fe2483acb5a2f2825c3c75
Phantola <phantoladev@gmail.com>
2023-10-23 15:58:06 +0900

    Delete un-used compo

--- src/HelloWorld.tsx
+++
@@ -1,9 +0,0 @@
-export function HelloWorld() {
-  const name = 'Phantola';
-  const style = {
-    'background-color': '#2c4f7c',
-    color: '#fff',
-  };
-
-  return <div style={style}>Hello {name}!</div>;
-}
